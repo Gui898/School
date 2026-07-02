47213852cb21032414ef5c01fdad45b400b77d7d
Gui898 <guihg2718@gmail.com>
2026-07-02 20:47:49 -0300

    feat: making login and register

---
+++ Activity5 (JS)/script/enter.js
@@ -0,0 +1,60 @@
+function validateRegister(event){
+    event.preventDefault()
+
+    const username_ = document.getElementById("username_register").value;
+    const email_ = document.getElementById("email_register").value;
+    const password_ = document.getElementById("password_register").value;
+
+    const obj = {
+        username: username_,
+        email: email_,
+        password: password_
+    };
+
+    let localData = localStorage.getItem("users_db");
+    if(!localData){
+        localData = JSON.stringify({users: []});
+    }
+
+    const data = JSON.parse(localData);
+
+    data.users.push(obj);
+    localStorage.setItem("users_db", JSON.stringify(data, null, 2));
+
+    alert("The user was registered!");
+    window.location.href = "../hub/store.html";
+}
+
+function login(event){
+    event.preventDefault()
+
+    const username_ = document.getElementById("username_login").value;
+    const password_ = document.getElementById("password_login").value;
+
+    const obj = {
+        username: username_,
+        password: password_
+    };
+
+    let localData = localStorage.getItem("users_db");
+    if(!localData){
+        alert("User not found");
+        throw new Error("User not found");
+    }
+
+    const data = JSON.parse(localData);
+
+    console.log("cuzao");
+
+    data.users.forEach(user => {
+        console.log(user.username);
+        if(user.username == username_ && user.password == password_){
+            window.location.href = "../hub/store.html";
+        }else{
+            alert("Incorrect username or password");
+            throw new Error("Incorrect username or password");
+        }
+    });
+
+}
+
